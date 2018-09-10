4347e6db9123c76b95b9f65986bc4c2fb36345b6
Chris Toal <ctoal@bink.com>
2018-09-10 17:13:33 +0100

    Toggle show/hide elements based on what 'type' is selected in 'security configuration'

---
+++ config_service/static/show_fields.js
@@ -0,0 +1,31 @@
+(function($) {
+    $(document).ready(function() {
+
+        if (!$) {
+            $ = django.jQuery;
+        }
+
+        var type_field = $('#id_securityservice_set-0-securitycredential_set-0-type');
+        var toggle_elements = $('.key-to-store, .field-key_to_store, .upload-button, .select')
+
+        var values = ['bink_private_key', 'bink_public_key', 'merchant_public_key', 'compound_key']
+
+        function toggleElements(value) {
+            if ($.inArray(value, values) !== -1) {
+                toggle_elements.show();
+            } else {
+                toggle_elements.hide();
+            }
+        }
+
+        //show/hide on load based on pervious value of selectField
+        toggleElements(type_field.val());
+
+        // show/hide on change
+        type_field.change(function() {
+            toggleElements($(this).val());
+        });
+
+        type_field.trigger('change');
+    })
+})(django.jQuery);
