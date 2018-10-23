39ef7d3bfadf24140827c3bd1a2faf9289906f41
Christian Prior <cprior@bink.com>
2018-10-23 16:02:59 +0100

    fix static_url in settings.py

--- europa/settings.py
+++ europa/settings.py
@@ -99,7 +99,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'europa.wsgi.application'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = env_var('STATIC_URL', '/static/')
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
